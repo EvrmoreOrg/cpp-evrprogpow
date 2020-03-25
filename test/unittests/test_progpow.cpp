058f5457f05792c39e1a82a6617ab4f4bbebfad3
blondfrogs <jeremy.anderson.utah@gmail.com>
2020-03-25 09:51:13 -0600

    Loop from 256 -> 516 for dag gen, fix unit tests

--- test/unittests/test_progpow.cpp
+++ test/unittests/test_progpow.cpp
@@ -32,9 +32,10 @@ TEST(progpow, l1_cache)
         cache_slice[i] = ethash::le::uint32(context.l1_cache[i]);
 
     const std::array<uint32_t, test_size> expected{
-        {690150178, 1181503948, 2248155602, 2118233073, 2193871115, 1791778428, 1067701239,
-            724807309, 530799275, 3480325829, 3899029234, 1998124059, 2541974622, 1100859971,
-            1297211151, 3268320000, 2217813733, 2690422980, 3172863319, 2651064309}};
+            {2492749011, 430724829, 2029256771, 3095580433, 3583790154, 3025086503,
+                    805985885, 4121693337, 2320382801, 3763444918, 1006127899, 1480743010,
+                    2592936015, 2598973744, 3038068233, 2754267228, 2867798800, 2342573634,
+                    467767296, 246004123}};
     EXPECT_EQ(cache_slice, expected);
 }
 
@@ -43,8 +44,8 @@ TEST(progpow, hash_empty)
     auto& context = get_ethash_epoch_context_0();
 
     const auto result = progpow::hash(context, 0, {}, 0);
-    const auto mix_hex = "40ce8bf6046c09f90f812f015d4ab8a1b504e7313e86d8a96197d5dadc3634e5";
-    const auto final_hex = "e6480cfa901dd209a9d8bef73275896be179f86b42e136efe692e14a41cb17b2";
+    const auto mix_hex = "6e97b47b134fda0c7888802988e1a373affeb28bcd813b6e9a0fc669c935d03a";
+    const auto final_hex = "e601a7257a70dc48fccc97a7330d704d776047623b92883d77111fb36870f3d1";
     EXPECT_EQ(to_hex(result.mix_hash), mix_hex);
     EXPECT_EQ(to_hex(result.final_hash), final_hex);
 }
@@ -59,8 +60,8 @@ TEST(progpow, hash_30000)
     auto context = ethash::create_epoch_context(ethash::get_epoch_number(block_number));
 
     const auto result = progpow::hash(*context, block_number, header, nonce);
-    const auto mix_hex = "d510f22b43969f8eefd9a8080bc2250ae6182c0abcbc05acf4c110ee306db152";
-    const auto final_hex = "26c8489150607aba6a81430a44d3b1526c205af431faa2a4ff9a029810f74d75";
+    const auto mix_hex = "177b565752a375501e11b6d9d3679c2df6197b2cab3a1ba2d6b10b8c71a3d459";
+    const auto final_hex = "c824bee0418e3cfb7fae56e0d5b3b8b14ba895777feea81c70c0ba947146da69";
     EXPECT_EQ(to_hex(result.mix_hash), mix_hex);
     EXPECT_EQ(to_hex(result.final_hash), final_hex);
 }
@@ -116,17 +117,17 @@ TEST(progpow, search)
     EXPECT_EQ(sr.final_hash, srl.final_hash);
     EXPECT_EQ(sr.nonce, srl.nonce);
 
-    sr = progpow::search(ctx, 0, {}, boundary, 0, 100);
-    srl = progpow::search_light(ctxl, 0, {}, boundary, 0, 100);
+    sr = progpow::search(ctx, 0, {}, boundary, 300, 100);
+    srl = progpow::search_light(ctxl, 0, {}, boundary, 300, 100);
 
     EXPECT_NE(sr.mix_hash, ethash::hash256{});
     EXPECT_NE(sr.final_hash, ethash::hash256{});
-    EXPECT_EQ(sr.nonce, 5);
+    EXPECT_EQ(sr.nonce, 395);
     EXPECT_EQ(sr.mix_hash, srl.mix_hash);
     EXPECT_EQ(sr.final_hash, srl.final_hash);
     EXPECT_EQ(sr.nonce, srl.nonce);
 
-    auto r = progpow::hash(ctx, 0, {}, 5);
+    auto r = progpow::hash(ctx, 0, {}, 395);
     EXPECT_EQ(sr.final_hash, r.final_hash);
     EXPECT_EQ(sr.mix_hash, r.mix_hash);
 }
